bc62e0ab73c4e61a2f4d5377da59e1c27b08a329
TakumaNakamura <91472318+tkmJackie@users.noreply.github.com>
2026-06-17 14:28:21 +0900

    Fix syntax errors in tools-config.js

--- tools-config.js
+++ tools-config.js
@@ -7,11 +7,11 @@ window.TOOLS_CONFIG = [
   {
     title: "画像サイズ調整ツール",
     url: "./image-resizer.html"
-  }
+  },
   {
     title: "[Blog]Markdown → HTML変換ツール",
     url: "./md-to-blog-converter.html"
-  }
+  },
   {
    title: "[お知らせ]Markdown → HTML変換ツール",
    url: "./md-to-information-converter.html"
